ea219f5ff103e1c3343b17397cc9ef4b930f1974
Harveym2012 <Harveym2012@yahoo.com>
2017-09-26 13:38:18 -0600

    Updated logic

--- scripts/topic1.js
+++ scripts/topic1.js
@@ -1,4 +1,6 @@
-function loop(countBy, countTimes){
+function loop(){
+     var countTimes = document.getElementById('countTimes').value;
+     var countBy = document.getElementById('countBy').value;
      for(int i = 0, i < countTimes, i++){
        countTotal += countBy;
      }
